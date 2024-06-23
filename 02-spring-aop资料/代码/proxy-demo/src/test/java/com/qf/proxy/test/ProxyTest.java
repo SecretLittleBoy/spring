a2f0abdd20d9e9cac69d48faa75cfc4810b22bab
LYH <3210101621@zju.edu.cn>
2024-06-23 18:48:17 +0800

    Refactor code to improve class constructors and lazy loading

--- 02-spring-aop资料/代码/proxy-demo/src/test/java/com/qf/proxy/test/ProxyTest.java
+++ 02-spring-aop资料/代码/proxy-demo/src/test/java/com/qf/proxy/test/ProxyTest.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 
 /**
  * @Author: 索尔 VX：214490523
- * @技术交流社区： qfjava.cn
+ *          @技术交流社区： qfjava.cn
  */
 public class ProxyTest {
 
@@ -16,7 +16,7 @@ public class ProxyTest {
      * 模拟租客
      */
     @Test
-    public void testRent(){
+    public void testRent() {
         HomeOwner homeOwner = new HomeOwner();
         homeOwner.rent();
     }
@@ -25,7 +25,7 @@ public class ProxyTest {
      * 使用中介租房
      */
     @Test
-    public void testRentByProxy(){
+    public void testRentByProxy() {
         HomeOwnerPlus homeOwnerPlus = new HomeOwnerPlus();
         HomeProxy homeProxy = new HomeProxy(homeOwnerPlus);
         homeProxy.rent();
@@ -35,7 +35,7 @@ public class ProxyTest {
      * 使用更强大的中介租房
      */
     @Test
-    public void testRentByProxyPlus(){
+    public void testRentByProxyPlus() {
         HomeOwnerPlus homeOwnerPlus = new HomeOwnerPlus();
         HomeProxy2 homeProxy = new HomeProxy2(homeOwnerPlus);
         homeProxy.rent();
