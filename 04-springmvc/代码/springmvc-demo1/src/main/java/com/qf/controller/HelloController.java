6982372f907869f8f5fd742df2a1a5350aa526ed
LYH <3210101621@zju.edu.cn>
2024-07-07 14:19:58 +0800

    update

--- 04-springmvc/代码/springmvc-demo1/src/main/java/com/qf/controller/HelloController.java
+++ 04-springmvc/代码/springmvc-demo1/src/main/java/com/qf/controller/HelloController.java
@@ -6,15 +6,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 /**
  * @Author: 索尔
  */
-@Controller //声明一个控制器
-@RequestMapping("/hello") //访问路径
+@Controller // 声明一个控制器
+@RequestMapping("/hello") // 访问路径
 public class HelloController {
-
-    @RequestMapping("/test1") //web接口
-    public String hello(){
+    @RequestMapping("/test1") // web接口
+    public String hello() {
         System.out.println("hello world");
         return "redirect:/index.jsp";
-
     }
 
 }
