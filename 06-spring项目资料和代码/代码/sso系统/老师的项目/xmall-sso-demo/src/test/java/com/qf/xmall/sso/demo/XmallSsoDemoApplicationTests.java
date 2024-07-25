8de88ef6904074be9b81896b05b3c7942fd9aaf6
LYH <3210101621@zju.edu.cn>
2024-07-25 23:58:02 +0800

    add test

--- 06-spring项目资料和代码/代码/sso系统/老师的项目/xmall-sso-demo/src/test/java/com/qf/xmall/sso/demo/XmallSsoDemoApplicationTests.java
+++ 06-spring项目资料和代码/代码/sso系统/老师的项目/xmall-sso-demo/src/test/java/com/qf/xmall/sso/demo/XmallSsoDemoApplicationTests.java
@@ -2,20 +2,18 @@ package com.qf.xmall.sso.demo;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import com.qf.xmall.sso.demo.common.exception.SSOException;
 import com.qf.xmall.sso.demo.controller.SSOController;
 import com.qf.xmall.sso.demo.common.result.ResultModel;
 import com.qf.xmall.sso.demo.common.dto.LoginResponse;
@@ -45,12 +43,12 @@ class XmallSsoDemoApplicationTests {
             assertEquals(200, res.getResponse().getStatus());
             String resModelStr = res.getResponse().getContentAsString();
             ResultModel<LoginResponse> resModel = new ObjectMapper().readValue(resModelStr, new TypeReference<ResultModel<LoginResponse>>() {});
-            assertEquals(resModel.getCode(), 500);
+            assertEquals(500, resModel.getCode());
         });
     }
 
     @Test
-    void loginAdminTest() {
+    void loginLogoutTest() {
         assertDoesNotThrow(() -> {
             MvcResult res = this.loginControllerMock.perform(MockMvcRequestBuilders.post("/sso/login")
                     .content("{\"userName\":\"test\",\"userPwd\":\"test\"}")
@@ -59,7 +57,34 @@ class XmallSsoDemoApplicationTests {
             assertEquals(200, res.getResponse().getStatus());
             String resModelStr = res.getResponse().getContentAsString();
             ResultModel<LoginResponse> resModel = new ObjectMapper().readValue(resModelStr, new TypeReference<ResultModel<LoginResponse>>() {});
-            assertEquals(resModel.getCode(), 1000);
+            assertEquals(1000, resModel.getCode());
+
+            String token = resModel.getResult().getToken();
+            assertNotNull(token);
+            assertTrue(token.length() > 0);
+            
+            MvcResult res2 = this.loginControllerMock.perform(MockMvcRequestBuilders.get("/sso/checkLogin")
+                    .param("token", token))
+                    .andReturn();
+            String resModelStr2 = res2.getResponse().getContentAsString();
+            ResultModel<LoginResponse> resModel2 = new ObjectMapper().readValue(resModelStr2, new TypeReference<ResultModel<LoginResponse>>() {});
+            assertEquals(1000, resModel2.getCode());
+            assertEquals(1, resModel2.getResult().getState());
+
+            MvcResult res3 = this.loginControllerMock.perform(MockMvcRequestBuilders.get("/sso/loginOut")
+                    .param("token", token))
+                    .andReturn();
+            String resModelStr3 = res3.getResponse().getContentAsString();
+            ResultModel resModel3 = new ObjectMapper().readValue(resModelStr3, new TypeReference<ResultModel>() {});
+            assertEquals(1000, resModel3.getCode());
+            
+            MvcResult res4 = this.loginControllerMock.perform(MockMvcRequestBuilders.get("/sso/checkLogin")
+                    .param("token", token))
+                    .andReturn();
+            String resModelStr4 = res4.getResponse().getContentAsString();
+            ResultModel<LoginResponse> resModel4 = new ObjectMapper().readValue(resModelStr4, new TypeReference<ResultModel<LoginResponse>>() {});
+            assertEquals(1000, resModel4.getCode());
+            assertEquals(0, resModel4.getResult().getState());
         });
     }
 }
